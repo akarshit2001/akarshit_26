e84f4b3d3bba94c9cf10a35714d18eea0510397a
akarshit2001 <akarshitsrivastava@gmail.com>
2020-11-09 19:45:10 +0530

    portfoliostart

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-$(function () {
-
-    $("#butto").blur(function (event) {
-        var screenWidth = window.innerWidth;
-        if (screenWidth < 768) {
-            $("#navbarSupportedContent").collapse('hide');
+var text= document.getElementById('head')
+function welcome_text(){
+    const  message="Hi! Viewer"
+    let i=0
+    const type=()=> {
+        if (i < message.length) {
+            text.innerHTML += message.charAt(i)
+            i++
+            setTimeout(type, 500)
+        } else {
+            let b = " |"
+            text.innerHTML += b.blink()
         }
-    });
+    }
+    type()
 
-    $("#butto").click(function (event) {
-        $(event.target).focus();
-    });
-});
+
+}
+welcome_text()
