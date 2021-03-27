57b81ab4d244901d655d68b3e13f5835b41f1773
akarshit2001 <akarshitsrivastava@gmail.com>
2021-03-27 21:16:20 +0530

    portfolioupdate

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-var text= document.getElementById('head')
-function welcome_text(){
-    const  message="Hi! Viewer"
-    let i=0
-    const type=()=> {
-        if (i < message.length) {
-            text.innerHTML += message.charAt(i)
-            i++
-            setTimeout(type, 500)
-        } else {
-            let b = " |"
-            text.innerHTML += b.blink()
-        }
-    }
-    type()
+// var text= document.getElementById('head')
+// function welcome_text(){
+//     const  message="Hi! Viewer"
+//     let i=0
+//     const type=()=> {
+//         if (i < message.length) {
+//             text.innerHTML += message.charAt(i)
+//             i++
+//             setTimeout(type, 500)
+//         } else {
+//             let b = " |"
+//             text.innerHTML += b.blink()
+//         }
+//     }
+//     type()
+//
+//
+// }
+// welcome_text()
+var ty = new Typed('#typed',{
+    strings : ['Hola!  <strong style="color: blueviolet">Welcome Viewer</strong>'],
+    typeSpeed:200,
+    backDelay: 900,
+    backSpeed: 30,
+    loop : true
 
 
-}
-welcome_text()
+})
